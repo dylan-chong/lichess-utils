7b778d93280913bab71d5843c8153512bbc62060
Dylan Chong <dylan.chong@icloud.com>
2025-05-29 15:09:39 +1200

    add copy script

---
+++ copy-to-icloud-userscripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ex
+
+cp ./lichess-board-speaker.js '/Users/Dylan/Library/Mobile Documents/com~apple~CloudDocs/Userscripts/lichess-board-speaker.user.js'
+
+echo "Copy done at $(date)"
