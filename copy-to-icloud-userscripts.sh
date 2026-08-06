f1e9f8f172239e191e02671def4a27b671cdcb7e
Dylan Chong <dylan.chong@icloud.com>
2026-08-06 23:26:43 +1200

    DEL: copy-to-icloud-userscripts.sh

--- copy-to-icloud-userscripts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cp ./lichess-board-speaker.user.js '/Users/Dylan/Library/Mobile Documents/com~apple~CloudDocs/Userscripts/lichess-board-speaker.user.js'
-
-echo "Copy done at $(date)"
