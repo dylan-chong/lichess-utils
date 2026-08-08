905bc1909ae1aeb5fd0fd569a83e4db45e7e0c7f
Dylan Chong <dylan.chong@icloud.com>
2026-08-08 14:33:34 +1200

    remove superpower crap

--- lichess-board-speaker.user.js
+++ lichess-board-speaker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        lichess-board-speaker
 // @description Blindfold chess training tool for lichess.org
-// @version     4.6.7
+// @version     4.6.8
 // @match       *://lichess.org/*
 // @require     https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js
 // @grant       none
